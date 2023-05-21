8957dd1f6a5e88ac7e51f854a3f909fcdc5d2901
Tarabass <p.rietveld@live.com>
2023-05-22 00:31:52 +0200

    Add CharacterCard to individual page

--- pages/characters/[id].vue
+++ pages/characters/[id].vue
@@ -1,6 +1,63 @@
 <template>
-    <div>{{ route.params.id }}</div>
+	<CharacterCard
+		:id="data.character.id"
+		:name="data.character.name"
+		:image="data.character.image"
+		:status="data.character.status"
+		:species="data.character.species"
+		:location="data.character.location.name"
+	/>
+	<NuxtLink :to="'/characters'">Go back</NuxtLink>
 </template>
 <script lang="ts" setup>
 const route = useRoute()
+
+type CharacterResult = {
+	character: {
+		id: string
+		name: string
+		image: string
+		status: string
+		species: string
+		location: {
+			name: string
+		}
+	}
+}
+
+// const query = gql`
+// 	query getCharacter($id: ID! = ${route.params.id}) {
+//         character(id: $id) {
+//         name
+//         image
+//         status
+//         id
+//         species
+//         location {
+//             name
+//         }
+//         }
+//     }
+// `
+
+// const { data } = await useAsyncQuery<CharacterResult>(query)
+
+const query = gql`
+	query getCharacter($id: ID!) {
+		character(id: $id) {
+			name
+			image
+			status
+			id
+			species
+			location {
+				name
+			}
+		}
+	}
+`
+
+const { data } = await useAsyncQuery<CharacterResult>(query, {
+	id: route.params.id,
+})
 </script>
